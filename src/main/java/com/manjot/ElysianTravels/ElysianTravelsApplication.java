f09c47e09bd999e333bd3759612caa5f28193079
KaurManjot0432 <0432manjot@gmail.com>
2023-12-30 16:24:54 +0530

    initial commit

---
+++ src/main/java/com/manjot/ElysianTravels/ElysianTravelsApplication.java
@@ -0,0 +1,13 @@
+package com.manjot.ElysianTravels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElysianTravelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElysianTravelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manjot/ElysianTravels/ElysianTravelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manjot.ElysianTravels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElysianTravelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
